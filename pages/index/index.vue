079db23931fdfb068fd01f38f8f0b6da0160ecba
kusaki <2466286500@qq.com>
2024-02-21 22:24:38 +0800

    实现食谱数据生成

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,66 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view v-for="item in recipeList" :key="item.day">
+			<view>
+				<text>{{item.day}}</text>
+			</view>
+			<view>
+				<view>
+					<text class="title">午餐：</text>
+				</view>
+				<text class="title">{{ item.lunch.map(v => v.name).join() }}</text>
+			</view>
+			<view>
+				<view>
+					<text class="title">晚餐:</text>
+				</view>
+				<text class="title">{{ item.dinner.map(v => v.name).join() }}</text>
+			</view>
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup>
+import { ref } from 'vue'
+import { weightedShuffle } from '@/utils/utils'
 
-		},
-		methods: {
+// 荤菜列表
+const meatList = new Array(100).fill('').map((_, i) => ({ name: `肉${i}`, weight: 1 }))
+// 素材列表
+const vegetableList = new Array(100).fill('').map((_, i) => ({ name: `蔬菜${i}`, weight: 1 }))
 
-		}
-	}
+// 生成这一周的数据
+const weekList = [
+	{ day: '星期一', meatNum: 4, vegetableNum: 4 },
+	{ day: '星期二', meatNum: 4, vegetableNum: 4 },
+	{ day: '星期三', meatNum: 4, vegetableNum: 4 },
+	{ day: '星期四', meatNum: 4, vegetableNum: 4 },
+	{ day: '星期五', meatNum: 4, vegetableNum: 4 },
+	{ day: '星期六', meatNum: 3, vegetableNum: 3 },
+	{ day: '星期日', meatNum: 3, vegetableNum: 3 },
+]
+
+const recipeList = ref([])
+// 给菜单随机排序
+const shuffleMeatList = weightedShuffle(meatList)
+const shuffleVegetableList = weightedShuffle(vegetableList)
+
+// 根据每周每天的数量生成菜单
+const createRecipe = (weekList, meatList, vegetableList) => {
+	const recipeList = []
+	weekList.forEach(v => {
+		recipeList.push({
+			day: v.day,
+			lunch: [...meatList.splice(0, v.meatNum), ...vegetableList.splice(0, v.vegetableNum)],
+			dinner: [...meatList.splice(0, v.meatNum), ...vegetableList.splice(0, v.vegetableNum)]
+		})
+	});
+
+	return recipeList
+}
+
+recipeList.value = createRecipe(weekList, shuffleMeatList, shuffleVegetableList)
+
+console.log(recipeList.value);
+
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '/@/': resolve(__dirname, './'),
+    },
+  },
+});
